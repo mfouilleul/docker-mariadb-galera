858f6d668fa25034c5eb4d864afb3ff1a5caa963
Maxime Fouilleul <maxime.fouilleul@blablacar.com>
2018-07-19 11:25:18 +0200

    Clean logging (galera)

--- galera/prepare.sh
+++ galera/prepare.sh
@@ -13,6 +13,7 @@ POD_NAMESPACE="${POD_NAMESPACE:-"default"}"
 DNS_NAME="${DNS_NAME:-"cluster.local"}"
 SEEDS=$(seeds --discovery=kubedns --service="$CLUSTER_NAME.$POD_NAMESPACE.svc.$DNS_NAME")
 
+echo "[Galera] Preparing config for cluster: $CLUSTER_NAME"
 sed -i -e "s|^wsrep_node_name[[:space:]]*=.*$|wsrep_node_name = ${NODE_NAME}|" "${GALERA_CONF}"
 sed -i -e "s|^wsrep_node_address[[:space:]]*=.*$|wsrep_node_address = ${NODE_ADDRESS}|" "${GALERA_CONF}"
 sed -i -e "s|^wsrep_cluster_name[[:space:]]*=.*$|wsrep_cluster_name = ${CLUSTER_NAME}|" "${GALERA_CONF}"
@@ -20,7 +21,7 @@ sed -i -e "s|^wsrep_cluster_address[[:space:]]*=.*$|wsrep_cluster_address = gcom
 
 if [ -n "$SEEDS" ] && [ "$SEEDS" != "$NODE_NAME" ]; then
     mkdir -p "$DATADIR/mysql"
-    echo "*** [Galera] Joining cluster: $CLUSTER_NAME"
+    echo "[Galera] Joining cluster: $CLUSTER_NAME"
 else
-    echo "*** [Galera] Starting new cluster!"
+    echo "[Galera] Starting new cluster!"
 fi
